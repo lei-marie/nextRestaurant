3cdc623695d4abd40624e0d957b01f557cc0cec2
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-11-08 22:52:53 -0500

    update urls

---
+++ utils/urls.js
@@ -0,0 +1,6 @@
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+
+export const STRIPE_PK =
+  process.env.NEXT_PUBLIC_STRIPE_PK ||
+  "pk_test_51LrBlkCHrU3OVai19r1FsNlKM33DgFCnmkJ2hwqoypqc8w5BgT6Cq4Dr6tc8fC7BVUZC6LpiMdYjOqgqdeI51uMx00lXaIPrfM";
